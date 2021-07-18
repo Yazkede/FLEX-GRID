0bf4705179b816045ea30a2361a64b7cdeef929d
Alexandr Rybin <missclick79@gmail.com>
2021-07-18 21:20:13 +0300

    slick

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 1,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 1500,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    }
+    );
+});
