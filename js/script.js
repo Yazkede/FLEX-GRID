6f04ef4283814c53c60f78343f43b49f96ea19e8
Alexandr Rybin <missclick79@gmail.com>
2021-07-29 00:12:56 +0300

    burger

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    $('.header__burger').click(function () {
+        $('.header__burger, .header__menu, .header__center-block').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $('.header__menu').click(function () {
+        $('.header__burger, .header__menu, .header__center-block').removeClass('active');
+        $('body').removeClass('lock');
+    });
 
     $('.slider').slick({
         arrows: true,
